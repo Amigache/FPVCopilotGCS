46b1e8472bc3dcd2c743e33ba0d30a5d1d828fe7
Héctor Narbona <amigache@hotmail.com>
2026-01-31 23:51:18 +0100

    Servir front desde el backend

--- server/index.js
+++ server/index.js
@@ -37,8 +37,15 @@ io.on('connection', (socket) => {
 });
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
+
+// 👉 SERVIR EL FRONTEND
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 // API Routes
 app.get('/api/status', (req, res) => {
